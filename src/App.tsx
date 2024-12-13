ff12036fd79a3d359f80e82aaa308b007ff792d8
EtienneNoir <109135635+EtienneNoir@users.noreply.github.com>
2024-12-13 05:48:15 +0200

    Created a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 const App = () => {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      // Defining a layout for mobile devices and one for larger devices
+
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: '"nav nav" "aside main"', // Screens wider than 1024 px
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 };
 
 export default App;
