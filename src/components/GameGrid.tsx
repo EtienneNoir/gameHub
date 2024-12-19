ea529cb70ca11663ab48979c90a499856756f442
EtienneNoir <109135635+EtienneNoir@users.noreply.github.com>
2024-12-19 18:00:22 +0200

    Refactor: Extract a Query object

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -2,12 +2,12 @@ import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames, { Platform } from "../hooks/useGames";
 import GameCard from "./GameCard";
 import { Genre } from "../hooks/useGenre";
+import { GameQuery } from "../App";
 interface Props {
-  selectedGenre: Genre | null;
-  selectedPlatform: Platform | null;
+  gameQuery: GameQuery;
 }
-const GameGrid = ({ selectedGenre, selectedPlatform }: Props) => {
-  const { data, error, isLoading } = useGames(selectedGenre, selectedPlatform);
+const GameGrid = ({ gameQuery }: Props) => {
+  const { data, error, isLoading } = useGames(gameQuery);
 
   const skeletons = [1, 2, 3, 4, 5, 6];
 
